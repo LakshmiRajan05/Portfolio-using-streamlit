d61e47739bd8a6abd721ff0fdb127aaa70f07373
LakshmiRajan05 <lakshmi.pushpakam@gmail.com>
2025-04-03 16:16:46 +0530

    update image

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ elif page == "Projects":
     # Project 1
     st.subheader("📌 Customer segmentation using Clustering")
     st.write("Customer segmentation on a retail data using RFM Analysis. RFM (Recency, Frequency, Monetary) analysis is a popularly used technique in marketing to identify high-value customers based on the purchasing behaviour. Here we have used 3 clustering approaches; kmeans, hierarchical clustering and dbscan, and analyse which is effective for the given data")
-    st.image("images/kmeans.png")
     st.image("images/hierarchical_clustering.png")
     st.link_button("View on GitHub", "https://github.com/LakshmiRajan05/Customer-Segmentation-using-Clustering")
 
